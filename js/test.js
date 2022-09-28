1067bcf6093081f630af06a3738c4a4ca7722bb7
Oleksii Nedohybchenko <104201289+aleksnedo@users.noreply.github.com>
2022-09-28 11:28:49 +0300

    Update test.js

--- js/test.js
+++ js/test.js
@@ -466,31 +466,3 @@
 //   btn.removeEventListener("click", handleClick);
 //   console.log("click event listener was removed from btn");
 // });
-
-const date = new Date("March 16, 2030 14:25:00");
-
-date.toString();
-// "Sat Mar 16 2030 14:25:00 GMT+0200 (Eastern European Standard Time)"
-
-date.toTimeString();
-// "14:25:00 GMT+0200 (Eastern European Standard Time)"
-
-date.toLocaleTimeString();
-// "2:25:00 PM"
-
-date.toUTCString();
-// "Sat, 16 Mar 2030 12:25:00 GMT"
-
-date.toDateString();
-// "Sat Mar 16 2030"
-
-date.toISOString();
-// "2030-03-16T12:25:00.000Z"
-
-date.toLocaleString();
-// "3/16/2030, 2:25:00 PM"
-
-date.getTime();
-// 1899894300000
-
-console.log(date.getTime());
